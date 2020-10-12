f00b3cb42312133a9c1bf914c84847eb59ea7053
Lor1138 <54803384+Lor1138@users.noreply.github.com>
2020-10-12 08:14:55 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,41 +1,27 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
-******************************************/
+//Store students and access them
+const studentItems = document.querySelectorAll(".student-item");
+
+let pageNumber = 2;
+
+
+//Show only 10 students per page. 
+const showPage = (list, page) => {
    
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
+    let start = (page * 10) - 11;
+    let end = page + 10;    
+    for(let i = 0; i < list.length; i++) { 
+      if(start >= i && i <= end ){
+        //to show students if true
+        list[i].style.display = 'block';
+      } else {
+          //to hide students if falses
+          list[i].style.display = 'none';
+      }
+    }
+    return;
+};
    
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-
-
-
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
-
+   
 
 
 /*** 
@@ -44,6 +30,47 @@ FSJS project 2 - List Filter and Pagination
 ***/
 
 
+const appendPageLinks = (list) => {
+   const pageNum = Math.ceil(list.length/10);
+   const pageContent = document.createElement('div');
+   const ul = document.createElement('ul');
+   const li = document.createElement('li');
+   const a = document.createElement('a');
+   pageContent.className = "page"; 
+   li.appendChild(a);
+   ul.appendChild(li);  
+   div.appendChild(ul);
+
+   for(let i = 0; i < pageNum; i++){
+      
+      
+      }
+   }
+   
+   /*** 
+   create a loop that uses the pageNum variable to interate the correct number of LI 
+   elements
+   create a nested UL element containing one LI for every ten students on the 
+   list.
+   Each LI needs to contain an A element with and HREF attribute of #.
+   the text also needs to be set to 1, 2 etc.
+   Looping this would be wise as well.
+
+   Add active class name to the first pagination link INITIALLY.
+   Add a "click" event listening to each A element LOOP THIS TOO.
+
+   The active class name should be added to the link that was just clicked.
+   The target property of the event object should be useful here.
+
+   The function to show a page should be called, passing in as arguments, 
+   the global variable for the list items, and the page number that should be shown. 
+   The text content of the A element that was just clicked can be helpful here.
+
+   **/
+
+   
+
+  
 
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+
