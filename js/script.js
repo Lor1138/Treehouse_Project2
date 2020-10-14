83e7cf1a5e84618e28921b51331ba87d5e645d90
Lor1138 <54803384+Lor1138@users.noreply.github.com>
2020-10-13 22:38:22 -0400

    Refined the functions

--- js/script.js
+++ js/script.js
@@ -1,27 +1,24 @@
 //Store students and access them
-const studentItems = document.querySelectorAll(".student-item");
-
-let pageNumber = 2;
+const studentItems = document.querySelectorAll(".student-list");
 
 
 //Show only 10 students per page. 
-const showPage = (list, page) => {
+function showPage (list, page) {
    
-    let start = (page * 10) - 11;
-    let end = page + 10;    
+    let start = page * 10 - 10;
+    let end = page * 10;    
     for(let i = 0; i < list.length; i++) { 
-      if(start >= i && i <= end ){
+      if(i >= start && i < end ){
         //to show students if true
         list[i].style.display = 'block';
       } else {
-          //to hide students if falses
+          //to hide students if false
           list[i].style.display = 'none';
       }
+   
     }
-    return;
+   
 };
-   
-   
 
 
 /*** 
@@ -30,47 +27,44 @@ const showPage = (list, page) => {
 ***/
 
 
-const appendPageLinks = (list) => {
+function appendPageLinks(list) {
    const pageNum = Math.ceil(list.length/10);
+   const pageList = document.getElementsByClassName('.page');
    const pageContent = document.createElement('div');
-   const ul = document.createElement('ul');
-   const li = document.createElement('li');
-   const a = document.createElement('a');
-   pageContent.className = "page"; 
-   li.appendChild(a);
-   ul.appendChild(li);  
-   div.appendChild(ul);
+   const pageUL = document.createElement('ul');
+   pageList.appendChild(pageContent);
+   pageContent.className = "pagination"; 
+   pageContent.appendChild(pageUL);
+   console.log(list);
+   
+      for(let i = 0; i < pageNum; i++){
+        const pageLink = document.createElement('a');
+        const paginationLi = document.createElement('li');
+        const pageLinkLabel = document.createTextNode(`${i+1}`)
+        a.appendChild(pageLinkLabel);
+        a.href = '#';
+        paginationLi.appendChild(pageLink);
+        pageUL.appendChild(paginationLi);
+        if (i === 0) {
+            pageLink.classList.add('active');
+        } else {
+            pageLink.classList.remove('active');
+        }
+      
 
-   for(let i = 0; i < pageNum; i++){
-      
-      
+      pageLink.addEventListener('click', (event) => { 
+         const pageNumbers = document.querySelectorAll('.pagination a');
+            showPage(list, pageLinkLabel);
+            for (let j = 0; j < pageNumbers.length; j++) {
+                pageNumbers[j].classList.remove('active');
+                event.target.classList.add('active');
+         }
+       });
       }
-   }
-   
-   /*** 
-   create a loop that uses the pageNum variable to interate the correct number of LI 
-   elements
-   create a nested UL element containing one LI for every ten students on the 
-   list.
-   Each LI needs to contain an A element with and HREF attribute of #.
-   the text also needs to be set to 1, 2 etc.
-   Looping this would be wise as well.
-
-   Add active class name to the first pagination link INITIALLY.
-   Add a "click" event listening to each A element LOOP THIS TOO.
-
-   The active class name should be added to the link that was just clicked.
-   The target property of the event object should be useful here.
-
-   The function to show a page should be called, passing in as arguments, 
-   the global variable for the list items, and the page number that should be shown. 
-   The text content of the A element that was just clicked can be helpful here.
-
-   **/
-
-   
-
   
+   }
 
-
+   showPage(studentItems, 1);
+   appendPageLinks(studentItems);
+   
 
