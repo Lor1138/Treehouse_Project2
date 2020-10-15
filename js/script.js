225e291ba75669c82e42cf07ce7a690c1496a9e7
Lor1138 <54803384+Lor1138@users.noreply.github.com>
2020-10-14 21:11:07 -0400

    Added empty string to page at line 33.

--- js/script.js
+++ js/script.js
@@ -9,10 +9,8 @@ function showPage (list, page) {
     let end = page * 10;    
     for(let i = 0; i < list.length; i++) { 
       if(i >= start && i < end ){
-        //to show students if true
         list[i].style.display = 'block';
       } else {
-          //to hide students if false
           list[i].style.display = 'none';
       }
    
@@ -28,19 +26,20 @@ function showPage (list, page) {
 
 
 function appendPageLinks(list) {
-   const pageNum = Math.ceil(list.length/10);
+   const studentsPerPage = Math.ceil(list.length/10);
    const pageList = document.getElementsByClassName('.page');
-   const pageContent = document.createElement('div');
+   const pageContent = document.createElement('div').classList.add('pagination');
    const pageUL = document.createElement('ul');
+   pageList.value = "";
    pageList.appendChild(pageContent);
-   pageContent.className = "pagination"; 
    pageContent.appendChild(pageUL);
-   console.log(list);
+
    
-      for(let i = 0; i < pageNum; i++){
+      for(let i = 0; i < studentsPerPage; i++){
         const pageLink = document.createElement('a');
         const paginationLi = document.createElement('li');
-        const pageLinkLabel = document.createTextNode(`${i+1}`)
+        const pageLinkLabel = document.createTextNode(`${i+1}`);
+        paginatinLi += studentItems/10;
         a.appendChild(pageLinkLabel);
         a.href = '#';
         paginationLi.appendChild(pageLink);
@@ -53,7 +52,7 @@ function appendPageLinks(list) {
       
 
       pageLink.addEventListener('click', (event) => { 
-         const pageNumbers = document.querySelectorAll('.pagination a');
+         const pageNumbers = document.getElementByTagName('a');
             showPage(list, pageLinkLabel);
             for (let j = 0; j < pageNumbers.length; j++) {
                 pageNumbers[j].classList.remove('active');
